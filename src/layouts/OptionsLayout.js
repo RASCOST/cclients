a0ad14a83c88ada57fa919b1078f9ff8045c41b9
RASC <rasc@sapo.pt>
2022-05-01 20:59:36 +0200

    created function exportDB in optionslayout

--- src/layouts/OptionsLayout.js
+++ src/layouts/OptionsLayout.js
@@ -2,8 +2,12 @@ import React from 'react'
 import { Text, View } from 'react-native'
 import RNFS from 'react-native-fs'
 import { TouchableOpacity } from 'react-native-gesture-handler'
+import {} from '../model/model';
 
 const OptionsLayout = () => {
+  const exportDB = () => {
+    const filePath = RNFS.DocumentDirectoryPath + '/clients.cvs'
+  }
 
   return (
     <View>
