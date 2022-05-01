7d661b78048b34dac6412e64e112bd8a58b5b05c
RASC <rasc@sapo.pt>
2022-05-01 18:19:18 +0200

    created the options file and function

---
+++ src/layouts/Options.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { View } from 'react-native'
+import RNFS from 'react-native-fs'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+
+const Options = () => {
+
+  return (
+    <View>
+      <TouchableOpacity>
+        Exporter BD
+      </TouchableOpacity>
+    </View>
+  )
+}
